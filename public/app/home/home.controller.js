a450292d756d4837c419714e2826d557fb4b2790
solongo89 <solongo.sumiya@gmail.com>
2016-10-03 18:12:04 +0200

    final

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -8,5 +8,15 @@
   /** @ngInject */
   function HomeController() {
     var vm = this;
+    vm.icon = [
+    './assets/home/ico-1.png',
+    './assets/home/ico-2.png',
+    './assets/home/ico-3.png',
+    './assets/home/ico-4.png',
+    './assets/home/ico-5.png',
+    './assets/home/ico-6.png',
+    './assets/home/ico-7.png',
+    './assets/home/ico-8.png',
+    ]
   }
 })();
